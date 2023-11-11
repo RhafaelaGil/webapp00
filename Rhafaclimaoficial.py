b38cc78d7a494863d514f355ad4709db679ddb5e
RhafaelaGil <149079307+RhafaelaGil@users.noreply.github.com>
2023-11-11 18:32:12 -0300

    Update Rhafaclimaoficial.py

--- Rhafaclimaoficial.py
+++ Rhafaclimaoficial.py
@@ -39,33 +39,33 @@ if st.button('Exibir Previsão'):
    
       # store the value corresponding
       # to the "temp" key of y
-      current_temperature = y["temp"]
+      current_temperatura = y["temp"]
    
       # store the value corresponding
       # to the "pressure" key of y
       current_pressure = y["pressure"]
    
       # store the value corresponding
-      # to the "humidity" key of y
-      current_humidity = y["humidade"]
+      # to the "humidade" key of y
+      current_humidade = y["humidade"]
    
       # store the value of "weather"
       # key in variable z
       z = x["weather"]
    
       # store the value corresponding 
-      # to the "description" key at 
+      # to the "descrição" key at 
       # the 0th index of z
-      weather_description = z[0]["descrição"]
+      weather_descrição = z[0]["descrição"]
    
       # print following values
-      st.write(" Temperature (in kelvin unit) = " +
-                      str(current_temperature) +
+      st.write(" Temperatura (in kelvin unit) = " +
+                      str(current_temperatura) +
             "\n atmospheric pressure (in hPa unit) = " +
                       str(current_pressure) +
-            "\n humidity (in percentage) = " +
+            "\n humidade (in percentage) = " +
                       str(current_humidade) +
-            "\n description = " +
+            "\n descrição = " +
                       str(weather_descrição))
    
   else:
